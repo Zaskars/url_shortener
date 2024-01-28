73a7abd7aa6ebb8a7c4a332de88f2a497dabc877
Timofey Bolotov <timbolotov3@gmail.com>
2024-01-29 00:44:32 +0300

    uuid field -> string filed

--- urlshortener/shortener/models.py
+++ urlshortener/shortener/models.py
@@ -4,7 +4,7 @@ import uuid
 
 class ShortenedURL(models.Model):
     original_url = models.URLField()
-    short_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    short_id = models.CharField(max_length=8, unique=True)
 
     def __str__(self):
         return self.original_url
--- urlshortener/shortener/utils.py
+++ urlshortener/shortener/utils.py
@@ -1,5 +1,9 @@
 import uuid
+import string
+import random
 
 
-def generate_short_id():
-    return str(uuid.uuid4())[:8]
+def generate_short_id(length=8):
+    # Generate a random string of letters and digits
+    short_id = ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+    return short_id
