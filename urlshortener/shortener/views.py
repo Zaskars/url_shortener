bae7d6498e21da2bb0a2edea192bdcd7ac5ba424
Timofey Bolotov <timbolotov3@gmail.com>
2024-01-29 04:48:17 +0300

    idk why swagger aint letting me to configure POST body...

--- urlshortener/shortener/views.py
+++ urlshortener/shortener/views.py
@@ -1,17 +1,23 @@
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework.parsers import JSONParser
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import ShortenedURL
 from .utils import generate_short_id
-from .serializers import ShortenedURLSerializer
+from .serializers import ShortenedURLSerializer, CreateURLSerializer
 from django.shortcuts import get_object_or_404, redirect
 
 
 class ShortenURLView(APIView):
+    parser_classes = [JSONParser]
     def post(self, request, *args, **kwargs):
-        original_url = request.data.get('url')
-        short_id = generate_short_id()
-        short_url = ShortenedURL.objects.create(original_url=original_url, short_id=short_id)
-        return Response(ShortenedURLSerializer(short_url).data)
+        serializer = CreateURLSerializer(data=request.data)
+        if serializer.is_valid():
+            original_url = serializer.validated_data['url']
+            short_id = generate_short_id()
+            short_url = ShortenedURL.objects.create(original_url=original_url, short_id=short_id)
+            return Response(ShortenedURLSerializer(short_url).data)
+        return Response(serializer.errors, status=400)
 
 
 class RedirectView(APIView):
