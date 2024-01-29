2e6e6e6d356ee127bb9b8e039f9aa5d9df0bda86
Timofey Bolotov <timbolotov3@gmail.com>
2024-01-29 04:47:29 +0300

    +swagger ui

--- urlshortener/urlshortener/urls.py
+++ urlshortener/urlshortener/urls.py
@@ -15,11 +15,30 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from rest_framework import permissions
+
+from django.urls import re_path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
 from shortener.views import RedirectView
 from shortener.views import ShortenURLView
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API",
+        default_version='v1.3.3.7',
+        description="API",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('shorten/', ShortenURLView.as_view(), name='shorten-url'),
-    path('r/<short_id>/', RedirectView.as_view(), name='redirect')
+    path('r/<short_id>/', RedirectView.as_view(), name='redirect'),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
