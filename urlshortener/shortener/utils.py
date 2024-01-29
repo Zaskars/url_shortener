dc6772f61896d2e1c6f44f582e0919884f3e65c3
Timofey Bolotov <timbolotov3@gmail.com>
2024-01-29 05:32:34 +0300

    +url validator

--- urlshortener/shortener/utils.py
+++ urlshortener/shortener/utils.py
@@ -1,9 +1,24 @@
 import uuid
 import string
 import random
+from urllib.parse import urlparse
+from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
 
 
 def generate_short_id(length=8):
-    # Generate a random string of letters and digits
     short_id = ''.join(random.choices(string.ascii_letters + string.digits, k=length))
     return short_id
+
+
+def normalize_and_validate_url(url):
+    if not urlparse(url).scheme:
+        url = 'http://' + url
+
+    validate = URLValidator()
+    try:
+        validate(url)
+    except ValidationError:
+        return None
+
+    return url
