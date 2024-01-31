892df77a77e9989f25a3594823cb44691b16fd15
Timofey Bolotov <timbolotov3@gmail.com>
2024-02-01 02:37:11 +0300

    +APIView -> GenericAPIView due to drf_spectacular is just better

--- urlshortener/shortener/views.py
+++ urlshortener/shortener/views.py
@@ -1,30 +1,33 @@
 from django.contrib.auth import authenticate
-from drf_yasg import openapi
-from drf_yasg.utils import swagger_auto_schema
+from drf_spectacular import openapi
 from rest_framework import status
+from rest_framework.generics import GenericAPIView
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+
 from .models import ShortenedURL
 from .utils import generate_short_id, normalize_and_validate_url
-from .serializers import ShortenedURLSerializer, UserRegistrationSerializer
+from .serializers import ShortenedURLSerializer, UserRegistrationSerializer, UserLoginSerializer, URLInputSerializer
 from django.shortcuts import get_object_or_404, redirect
 
 
-class ShortenURLView(APIView):
-    @swagger_auto_schema(request_body=openapi.Schema(
-        type=openapi.TYPE_OBJECT,
-        properties={
-            'url': openapi.Schema(type=openapi.TYPE_STRING, description='string'),
-        }
-    ))
+class ShortenURLView(GenericAPIView):
+    serializer_class = URLInputSerializer
+
     def post(self, request, *args, **kwargs):
-        original_url = request.data.get('url')
-        normalized_url = normalize_and_validate_url(original_url)
-        if not normalized_url:
-            return Response({'error': 'Invalid URL'}, status=status.HTTP_400_BAD_REQUEST)
-        short_id = generate_short_id()
-        short_url = ShortenedURL.objects.create(original_url=normalized_url, short_id=short_id)
-        return Response(ShortenedURLSerializer(short_url).data)
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            original_url = serializer.validated_data['url']
+            normalized_url = normalize_and_validate_url(original_url)
+            print(normalized_url, original_url)
+            if not normalized_url:
+                return Response({'error': 'Invalid URL'}, status=status.HTTP_400_BAD_REQUEST)
+            short_id = generate_short_id()
+            short_url = ShortenedURL.objects.create(original_url=normalized_url, short_id=short_id, user=request.user)
+            return Response(ShortenedURLSerializer(short_url).data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class RedirectView(APIView):
@@ -33,47 +36,41 @@ class RedirectView(APIView):
         return redirect(short_url.original_url)
 
 
-class UserRegistrationView(APIView):
-    @swagger_auto_schema(
-        request_body=openapi.Schema(
-            type=openapi.TYPE_OBJECT,
-            required=['username', 'password', 'email'],
-            properties={
-                'username': openapi.Schema(type=openapi.TYPE_STRING, description='User\'s username'),
-                'password': openapi.Schema(type=openapi.TYPE_STRING, description='User\'s password'),
-                'email': openapi.Schema(type=openapi.TYPE_STRING, description='User\'s email'),
-            },
-        ),
-        responses={
-            201: openapi.Response('User created successfully')
-        }
-    )
-    def post(self, request):
-        serializer = UserRegistrationSerializer(data=request.data)
+class UserRegistrationView(GenericAPIView):
+    serializer_class = UserRegistrationSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
-            return Response({'message': 'User created successfully'}, status=status.HTTP_201_CREATED)
+            user = serializer.save()
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+            }, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class UserLoginView(APIView):
-    @swagger_auto_schema(
-        request_body=openapi.Schema(
-            type=openapi.TYPE_OBJECT,
-            required=['username', 'password'],
-            properties={
-                'username': openapi.Schema(type=openapi.TYPE_STRING, description='User\'s username'),
-                'password': openapi.Schema(type=openapi.TYPE_STRING, description='User\'s password'),
-            },
-        ),
-        responses={
-            201: openapi.Response('User logged in successfully')
-        }
-    )
-    def post(self, request):
-        username = request.data.get('username')
-        password = request.data.get('password')
-        user = authenticate(username=username, password=password)
-        if user:
-            return Response({'message': 'User logged in successfully'})
-        return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+class UserLoginView(GenericAPIView):
+    serializer_class = UserLoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+            }, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_401_UNAUTHORIZED)
+
+
+class UserURLsView(GenericAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = ShortenedURLSerializer
+
+    def get(self, request, *args, **kwargs):
+        urls = ShortenedURL.objects.filter(user=request.user)
+        serializer = self.get_serializer(urls, many=True)
+        return Response(serializer.data)
