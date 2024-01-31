9bfdb0528b2579124673bf9251fb1d8983c27335
Timofey Bolotov <timbolotov3@gmail.com>
2024-02-01 00:59:04 +0300

    +login and reg views

--- urlshortener/shortener/views.py
+++ urlshortener/shortener/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
@@ -5,7 +6,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import ShortenedURL
 from .utils import generate_short_id, normalize_and_validate_url
-from .serializers import ShortenedURLSerializer
+from .serializers import ShortenedURLSerializer, UserRegistrationSerializer
 from django.shortcuts import get_object_or_404, redirect
 
 
@@ -30,3 +31,49 @@ class RedirectView(APIView):
     def get(self, request, short_id, *args, **kwargs):
         short_url = get_object_or_404(ShortenedURL, short_id=short_id)
         return redirect(short_url.original_url)
+
+
+class UserRegistrationView(APIView):
+    @swagger_auto_schema(
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            required=['username', 'password', 'email'],
+            properties={
+                'username': openapi.Schema(type=openapi.TYPE_STRING, description='User\'s username'),
+                'password': openapi.Schema(type=openapi.TYPE_STRING, description='User\'s password'),
+                'email': openapi.Schema(type=openapi.TYPE_STRING, description='User\'s email'),
+            },
+        ),
+        responses={
+            201: openapi.Response('User created successfully')
+        }
+    )
+    def post(self, request):
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': 'User created successfully'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserLoginView(APIView):
+    @swagger_auto_schema(
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            required=['username', 'password'],
+            properties={
+                'username': openapi.Schema(type=openapi.TYPE_STRING, description='User\'s username'),
+                'password': openapi.Schema(type=openapi.TYPE_STRING, description='User\'s password'),
+            },
+        ),
+        responses={
+            201: openapi.Response('User logged in successfully')
+        }
+    )
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username=username, password=password)
+        if user:
+            return Response({'message': 'User logged in successfully'})
+        return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
