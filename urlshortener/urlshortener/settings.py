fa8755ec46fea3b2be10ad7295bfd6d07fd9d084
Timofey Bolotov <timbolotov3@gmail.com>
2024-02-01 02:36:00 +0300

    drf_yasg -> drf_spectacular

--- urlshortener/urlshortener/settings.py
+++ urlshortener/urlshortener/settings.py
@@ -17,7 +17,6 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -29,7 +28,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -40,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "shortener",
-    "drf_yasg"
+    'drf_spectacular'
 ]
 
 MIDDLEWARE = [
@@ -73,6 +71,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "urlshortener.wsgi.application"
 
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
@@ -87,7 +91,6 @@ DATABASES = {
         "PORT": os.getenv("DB_PORT"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -107,7 +110,17 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'title',
+    'DESCRIPTION': 'desc',
+    'VERSION': '1.0.0',
+    'SECURITY': [
+        {
+            'bearerAuth': []
+        }
+    ],
+    'COMPONENT_SPLIT_REQUEST': True,
+}
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -118,7 +131,6 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
