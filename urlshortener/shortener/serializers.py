e0f89f44e5672bb6cbf21a423216afc7e95cc064
Timofey Bolotov <timbolotov3@gmail.com>
2024-02-01 00:58:05 +0300

    +serializer for user reg

--- urlshortener/shortener/serializers.py
+++ urlshortener/shortener/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import ShortenedURL
 
@@ -6,3 +7,15 @@ class ShortenedURLSerializer(serializers.ModelSerializer):
     class Meta:
         model = ShortenedURL
         fields = ['original_url', 'short_id']
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'email']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
