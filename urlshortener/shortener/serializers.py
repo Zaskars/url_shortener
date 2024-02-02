754af30f9875d34afc6a1cceefea0034c9641572
Timofey Bolotov <timbolotov3@gmail.com>
2024-02-02 23:28:37 +0300

    +serializer adj

--- urlshortener/shortener/serializers.py
+++ urlshortener/shortener/serializers.py
@@ -2,17 +2,32 @@ from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import ShortenedURL
+from .utils import normalize_and_validate_url
 
 
 class ShortenedURLSerializer(serializers.ModelSerializer):
+    original_url = serializers.CharField()
+
     class Meta:
         model = ShortenedURL
         fields = ['original_url', 'short_id']
+
+    def validate_original_url(self, value):
+        normalized_url = normalize_and_validate_url(value)
+        if not normalized_url:
+            raise serializers.ValidationError("Invalid URL")
+        return normalized_url
 
 
 class URLInputSerializer(serializers.Serializer):
     url = serializers.CharField()
     custom_short_id = serializers.CharField(max_length=8, required=False, allow_blank=True)
+
+    def validate_url(self, value):
+        normalized_url = normalize_and_validate_url(value)
+        if not normalized_url:
+            raise serializers.ValidationError("Invalid URL")
+        return normalized_url
 
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
