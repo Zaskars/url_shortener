54a803f624da19dd890a3bfabf6eea4378754079
Timofey Bolotov <timbolotov3@gmail.com>
2024-02-03 22:09:08 +0300

    -useless serializer + url handler

--- urlshortener/shortener/serializers.py
+++ urlshortener/shortener/serializers.py
@@ -1,33 +1,64 @@
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
+from rest_framework import serializers, status
+from .models import ShortenedURL
+from .utils import normalize_and_validate_url, generate_short_id
+
 from rest_framework import serializers
 from .models import ShortenedURL
-from .utils import normalize_and_validate_url
 
 
-class ShortenedURLSerializer(serializers.ModelSerializer):
-    original_url = serializers.CharField()
+def handle_url(original_url, custom_short_id=None, user=None, instance=None):
+    normalized_url = normalize_and_validate_url(original_url)
+    if not normalized_url:
+        return {'error': 'Invalid URL', 'status': status.HTTP_400_BAD_REQUEST}
+
+    if custom_short_id:
+        if ShortenedURL.objects.filter(short_id=custom_short_id).exists() and (instance is None or instance.short_id != custom_short_id):
+            return {'error': 'Custom short ID is already in use', 'status': status.HTTP_409_CONFLICT}
+        short_id = custom_short_id
+    else:
+        short_id = generate_short_id()
+
+    if instance is None:
+        short_url = ShortenedURL.objects.create(original_url=normalized_url, short_id=short_id, user=user)
+    else:
+        instance.original_url = normalized_url
+        instance.short_id = short_id
+        instance.save()
+        short_url = instance
+
+    return {'data': URLInputSerializer(short_url).data, 'status': status.HTTP_200_OK}
+
+
+class URLInputSerializer(serializers.ModelSerializer):
+    url = serializers.CharField(source='original_url', required=False)  # Переименовываем в url для API
+    custom_short_id = serializers.CharField(max_length=8, required=False, allow_blank=True)
 
     class Meta:
         model = ShortenedURL
-        fields = ['original_url', 'short_id']
-
-    def validate_original_url(self, value):
-        normalized_url = normalize_and_validate_url(value)
-        if not normalized_url:
-            raise serializers.ValidationError("Invalid URL")
-        return normalized_url
-
-
-class URLInputSerializer(serializers.Serializer):
-    url = serializers.CharField()
-    custom_short_id = serializers.CharField(max_length=8, required=False, allow_blank=True)
+        fields = ['url', 'custom_short_id', 'short_id']
+        extra_kwargs = {
+            'short_id': {'read_only': True}  # short_id только для чтения, его нельзя задать через API
+        }
 
     def validate_url(self, value):
+        # Проверка и нормализация URL
         normalized_url = normalize_and_validate_url(value)
         if not normalized_url:
             raise serializers.ValidationError("Invalid URL")
         return normalized_url
+
+    def validate(self, attrs):
+        return super().validate(attrs)
+
+    def create(self, validated_data):
+        return handle_url(**validated_data, user=self.context['request'].user)
+
+    def update(self, instance, validated_data):
+        # Логика обновления
+        return handle_url(instance=instance, **validated_data, user=self.context['request'].user)
+
 
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
